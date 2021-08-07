7e2efba2a383f38ae827f8eefe6ee8652bc11b2f
Pinak Thakkar <pinak1180@users.noreply.github.com>
2021-08-07 10:14:49 +0530

    Update snowflake-odbc.sh

--- .profile.d/snowflake-odbc.sh
+++ .profile.d/snowflake-odbc.sh
@@ -8,9 +8,12 @@ Driver=SnowflakeDSIIDriver
 Locale=en-US
 PORT=443
 SSL=on
+Query_Timeout=500
 CLIENT_SESSION_KEEP_ALIVE=true
 uid=${SNOWFLAKE_USERNAME}
 pwd=${SNOWFLAKE_PASSWORD}
 server=${SNOWFLAKE_SERVER}
 database=${SNOWFLAKE_DATABASE}
+account=${SNOWFLAKE_ACCOUNT}
+schema=${SNOWFLAKE_SCHEMA}
 " > ${ODBCSYSINI}/odbc.ini
