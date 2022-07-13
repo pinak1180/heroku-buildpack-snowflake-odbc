3fa3bdcb14e53b4b1e8b5cc903cd46ae814560fe
Pinak Thakkar <pinak1180@users.noreply.github.com>
2022-07-13 14:56:23 +0530

    Update snowflake-odbc.sh

--- .profile.d/snowflake-odbc.sh
+++ .profile.d/snowflake-odbc.sh
@@ -17,3 +17,19 @@ database=${SNOWFLAKE_DATABASE}
 account=${SNOWFLAKE_ACCOUNT}
 schema=${SNOWFLAKE_SCHEMA}
 " > ${ODBCSYSINI}/odbc.ini
+
+echo "[snowflake_master]
+Description=SnowflakeDB
+Driver=SnowflakeDSIIDriver
+Locale=en-US
+PORT=443
+SSL=on
+Query_Timeout=500
+CLIENT_SESSION_KEEP_ALIVE=true
+uid=${SNOWFLAKE_USERNAME_MASTER}
+pwd=${SNOWFLAKE_PASSWORD_MASTER}
+server=${SNOWFLAKE_SERVER}
+database=${SNOWFLAKE_DATABASE_MASTER}
+account=${SNOWFLAKE_ACCOUNT}
+schema=${SNOWFLAKE_SCHEMA_MASTER}
+" > ${ODBCSYSINI}/odbc.ini
