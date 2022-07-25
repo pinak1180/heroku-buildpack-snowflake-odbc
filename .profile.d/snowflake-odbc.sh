4a83e4da611033796466874a7db7b26e6c1137e6
Pinak Thakkar <pinak1180@users.noreply.github.com>
2022-07-25 15:29:32 +0530

    Update snowflake-odbc.sh

--- .profile.d/snowflake-odbc.sh
+++ .profile.d/snowflake-odbc.sh
@@ -2,24 +2,6 @@
 
 export ODBCSYSINI=${HOME}/.apt/usr/lib/snowflake/odbc/conf/
 
-echo "[snowflake]
-Description=SnowflakeDB
-Driver=SnowflakeDSIIDriver
-Locale=en-US
-PORT=443
-SSL=on
-Query_Timeout=500
-CLIENT_SESSION_KEEP_ALIVE=true
-uid=${SNOWFLAKE_USERNAME}
-pwd=${SNOWFLAKE_PASSWORD}
-server=${SNOWFLAKE_SERVER}
-database=${SNOWFLAKE_DATABASE}
-account=${SNOWFLAKE_ACCOUNT}
-schema=${SNOWFLAKE_SCHEMA}
-" > ${ODBCSYSINI}/odbc.ini
-
-
-
 echo "[snowflake_master]
 Description=SnowflakeDB
 Driver=SnowflakeDSIIDriver
@@ -34,4 +16,19 @@ server=${SNOWFLAKE_SERVER}
 database=${SNOWFLAKE_DATABASE_MASTER}
 account=${SNOWFLAKE_ACCOUNT}
 schema=${SNOWFLAKE_SCHEMA_MASTER}
+
+[snowflake]
+Description=SnowflakeDB
+Driver=SnowflakeDSIIDriver
+Locale=en-US
+PORT=443
+SSL=on
+Query_Timeout=500
+CLIENT_SESSION_KEEP_ALIVE=true
+uid=${SNOWFLAKE_USERNAME}
+pwd=${SNOWFLAKE_PASSWORD}
+server=${SNOWFLAKE_SERVER}
+database=${SNOWFLAKE_DATABASE}
+account=${SNOWFLAKE_ACCOUNT}
+schema=${SNOWFLAKE_SCHEMA}
 " > ${ODBCSYSINI}/odbc.ini
